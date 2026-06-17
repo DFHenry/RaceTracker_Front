99e07851c815a62cfbff2f828cb73eb55084a148
David Henry <david_freeman_henry@hotmail.com>
2026-06-16 23:19:26 -0400

    added racer countdown and stopwatch functionality

--- race_tracker_FrontEnd/src/components/RaceData/index.jsx
+++ race_tracker_FrontEnd/src/components/RaceData/index.jsx
@@ -4,23 +4,38 @@ import RacerReg from "../props/RacerReg/racerReg";
 
 export default function getRacingData()
 {
+    //api variables
     const [count, setCount] = useState(0);
     const [racingData, setRaceData] = useState("");
     const [racers, setRacers] = useState([]);
+
+    //countdown variables
+    const [countdown, setCountdown] = useState(10);
+    const [countdownOn, setCountDownOn] = useState(false);
+
+    //stopwatch variables
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
 
     const racerArray = [];
 
     useEffect(() =>
     {
-
         const getData = async () => 
         {
-            console.log("ping");
             let response = await fetch("http://localhost:8888/api/sendApi");
             let data = await response.json();
 
             setRaceData(data);
             setRacers(data.racers);
+
+            // if(getRacingData.raceState != "running")
+            // {
+            //     setCountDownOn(false);
+            //     setCountdown(10);
+            //     setIsRunning(false);
+            //     setTime(0);
+            // }
         }
         getData();
 
@@ -28,23 +43,83 @@ export default function getRacingData()
         {
             setCount((prevCount) => prevCount + 1);
             getData();
-        }, 1000);
-        //getData();
 
-        return () => clearInterval(timer);
-    }, []);
+        }, 750);
+
+        let countdownId;
+
+        let intervalId;
+
+        if(countdownOn)
+        {
+            countdownId = setInterval(() => setTime(countdown - 1), 1000);
+            console.log(countdown);
+        }
+
+        if(isRunning)
+        {
+            intervalId = setInterval(() => setTime(time + 1), 10);
+        }
+
+        return () => 
+            {
+                clearInterval(countdownId);
+                clearInterval(intervalId);
+                clearInterval(timer); 
+            }
+
+    }, [countdownOn, countdown, isRunning, time]);
+
+    const countdownTimer = Math.floor(countdown);
+    let goMessage = "";
+
+    const watchMinutes = Math.floor((time % 360000) / 6000);
+
+    const watchSeconds = Math.floor((time % 6000) / 100);
+
+    const watchMilliseconds = time % 100;
+
+    setTimeout(countDownGoDown, 1000);
+
+    function countDownGoDown()
+    {
+        setCountdown(countdown - 1);
+        if(countdown <= 1 && isRunning == false)
+        {
+            setIsRunning(true);
+        }
+    }
+
+    if(countdownTimer > 0)
+    {
+        goMessage = countdownTimer;
+        if(setCountDownOn == false)
+        {
+            countDownGoDown;
+        }
+    }
+    else if(countdownTimer <= 0 && countdownTimer >=-29)
+    {
+        // console.log("Stop Watch Started: " + countdownTimer.toString());
+        goMessage = "GO!";
+    }
+    else
+    {
+        goMessage = "";
+    }
 
     if(racingData.raceState == "standby")
     {
-    return(
-        <div>
-            <p>Standby For A New Race</p>
-            
-        </div>
-    )}
+        return(
+            <div>
+                <p>Standby For A New Race</p>
+            </div>
+        );
+    }
+
     else if(racingData.raceState == "registration")
     {
-        console.log(racerArray.racers);
+        //console.log(racerArray.racers);
         if(racerArray.length > racingData.racers.length)
         {
             racerArray.push(racingData.racers.length - 1);
@@ -55,8 +130,11 @@ export default function getRacingData()
                 <p>Registration is Active!</p>
                 <table>
                     <thead>
-                        <th>Racer Name</th>
-                        <th>Vehicle Number</th>
+                        <tr>
+                            <td>Racer Name</td>
+                            <td>Vehicle Number</td>
+                        </tr>
+
                     </thead>
                     <tbody>
                         {racers.map((m, i) =>
@@ -77,6 +155,29 @@ export default function getRacingData()
         return(
             <>
                 <p>Get Ready To Race!</p>
+            </>
+        )
+    }
+
+    else if(racingData.raceState == "running")
+    {
+        return(
+            <>
+                <p>Race Is Running</p>
+                <p>{goMessage}</p>
+                <p>{watchMinutes.toString().padStart(2, "0")}:
+                   {watchSeconds.toString().padStart(2, "0")}:
+                   {watchMilliseconds.toString().padStart(2, "0")} 
+                </p>
+            </>
+        );
+    }
+
+    else if (racingData.raceState == "finishing")
+    {
+        return(
+            <>
+                <p>Race Has Concluded</p>
             </>
         )
     }
